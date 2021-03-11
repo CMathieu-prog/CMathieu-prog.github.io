1d5b8b07bc3390379627de8fe2c2ca1ee4bb78ad
menthealeau <mathieu.couret@laposte.net>
2021-03-11 22:02:33 +0100

    update js list project

--- js/perso.js
+++ js/perso.js
@@ -10,5 +10,23 @@ $(document).ready(function() {
         $('html, body').animate({scrollTop: $(page).offset().top}, speed);
 
     });
+    $(".filter-b").click(function(){
+        var value = $(this).attr('data-filter');
+        if(value == "all")
+        { 
+            $('.filter').show('1000');
+        }
+        else
+        { 
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+        }
+    });
+    
+    if ($(".filter-b").removeClass("active")) {
+      $(this).removeClass("active");
+    }
+    $(this).addClass("active");
+    });
 });
 
